382bf778de1e8f4ce6ebe2adf9903c72b5932019
FrankBlack <fonyeabo12@gmail.com>
2020-09-07 01:23:37 -0500

    Updates

---
+++ precache-manifest.ad2f75cda3a07b2d7f980ec5f7255221.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "932135f5133dffb2cdb5",
+    "url": "/static/css/main.ccb118e9.chunk.css"
+  },
+  {
+    "revision": "932135f5133dffb2cdb5",
+    "url": "/static/js/main.cf0bd8ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "470a9934a328f8124c13",
+    "url": "/static/css/2.61319182.chunk.css"
+  },
+  {
+    "revision": "470a9934a328f8124c13",
+    "url": "/static/js/2.11710b8d.chunk.js"
+  },
+  {
+    "revision": "6df757bd93ac05cd248ed747aebab836",
+    "url": "/static/media/grey_background.6df757bd.jpg"
+  },
+  {
+    "revision": "1b11b86039c0b93788eb340c04f274d5",
+    "url": "/static/media/Photo.1b11b860.jpg"
+  },
+  {
+    "revision": "51a8eb9c0b4863069d5996b232946ca4",
+    "url": "/static/media/Covid 19 screenshot.51a8eb9c.jpg"
+  },
+  {
+    "revision": "2996b76a85dcb86de79407b160bd0169",
+    "url": "/static/media/Car Maintenance Logger.2996b76a.jpg"
+  },
+  {
+    "revision": "76ac8d4bc1c39d75414932dffdd83080",
+    "url": "/index.html"
+  }
+];
